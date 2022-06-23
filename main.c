abefe37077eb7143f2c68e84a619b8a4b7e4931e
Dsowerby4 <deaconsowerby8@gmail.com>
2022-06-22 18:49:25 -0700

    I think I forgot to upload this

--- main.c
+++ main.c
@@ -1,23 +1,18 @@
-//#define WIN32_LEAN_AND_MEAN 
 //Credit to Auld for Base
 #include <windows.h>
-//#include <dsound.h>
 #include <GL/gl.h>
-#include <stdio.h>
-#include <stdlib.h>
-//#include <GL/glu.h>
 #include "khrplatform.h"
 #include "glext.h"
-//#include "Test1.h"
 #include "shader_code.h" 
+#include "audio.h"
 
 typedef void (*GenFP)(void); // any function ptr type would do
 static GenFP glFP[22];
-const unsigned int SCR_WIDTH = 1280;
-const unsigned int SCR_HEIGHT = 720;
+const GLuint SCR_WIDTH = 1280; 
+const GLuint SCR_HEIGHT = 720;     
 
-const GLuint SH_WIDTH = 1280;
-const GLuint SH_HEIGHT = 720; 
+const GLuint SH_WIDTH = 1280;   
+const GLuint SH_HEIGHT = 720;    
 const GLuint SH_DEPTH = 3;//Extra Layer for Rendering Scene
 
 const static char* glnames[]={
@@ -26,70 +21,84 @@ const static char* glnames[]={
 	 "glCompileShader", "glCreateProgram", "glAttachShader", "glLinkProgram",
 	 "glGenFramebuffers", "glUseProgram", 
 	 "glBindFramebuffer", "glFramebufferTexture3D", 
-	 "glDispatchCompute", "glMemoryBarrier", "glBlitFramebuffer",
-	 "glUniform1f", "glUniform1i", "glGetUniformLocation"
+	 "glDispatchCompute", "glMemoryBarrier", "glBlitFramebuffer", 
+	 "glUniform1f", "glUniform1i", "glGetUniformLocation" 
 };
-
-static 	PIXELFORMATDESCRIPTOR pfd; 
+  
+static 	PIXELFORMATDESCRIPTOR pfd = {sizeof(pfd), 1, PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL|PFD_DOUBLEBUFFER, PFD_TYPE_RGBA, 32, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 32, 0, 0, PFD_MAIN_PLANE, 0, 0, 0, 0};  
 static DEVMODE dmScreenSettings;
-
-int WINAPI WinMainCRTStartup()
-{   
-
-	dmScreenSettings.dmSize=sizeof(dmScreenSettings);		
-	dmScreenSettings.dmPelsWidth	= 1280;
-	dmScreenSettings.dmPelsHeight= 720;
-	dmScreenSettings.dmFields=DM_PELSWIDTH|DM_PELSHEIGHT;
-	ChangeDisplaySettings(&dmScreenSettings,CDS_FULLSCREEN); 
+int WINAPI WinMainCRTStartup() 
+{       
  
-	pfd.cColorBits = pfd.cDepthBits = 32; 
-	pfd.dwFlags    = PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;	
-	PVOID hDC = GetDC ( CreateWindow("edit", 0, WS_POPUP|WS_VISIBLE|WS_MAXIMIZE, 0, 0, 0 , 0, 0, 0, 0, 0) );         
+	dmScreenSettings.dmSize=sizeof(dmScreenSettings);	 	  
+	dmScreenSettings.dmPelsWidth	= 1280; 
+	dmScreenSettings.dmPelsHeight= 720; 
+	dmScreenSettings.dmFields=DM_PELSWIDTH|DM_PELSHEIGHT;
+	ChangeDisplaySettings(&dmScreenSettings,CDS_FULLSCREEN);    
+ 
+	//pfd.cColorBits = pfd.cDepthBits = 32; 
+	//pfd.dwFlags    = PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;	  
+	ShowCursor(0); 	
+	PVOID hDC = GetDC ( CreateWindow((LPCSTR)0xC018, 0, WS_POPUP|WS_VISIBLE|WS_MAXIMIZE, 0, 0, 0 , 0, 0, 0, 0, 0) );         
 	SetPixelFormat ( hDC, ChoosePixelFormat ( hDC, &pfd) , &pfd );
-	wglMakeCurrent ( hDC, wglCreateContext(hDC) );
-	ShowCursor(FALSE);
+	wglMakeCurrent ( hDC, wglCreateContext(hDC) );    
 	
+	//ShowCursor(FALSE); 
+	
+	//Audio Stuff
+	CreateThread(0, 0, (LPTHREAD_START_ROUTINE)_4klang_render, lpSoundBuffer, 0, 0);
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &WaveFMT, NULL, 0, CALLBACK_NULL);
+	waveOutPrepareHeader(hWaveOut, &WaveHDR, sizeof(WaveHDR));
+	waveOutWrite(hWaveOut, &WaveHDR, sizeof(WaveHDR)); 
+	 
+	//GL Stuff
 	GLuint FBO;
 	GLuint tex_output;
 	int i;
 	for (i=0; i<22; i++) glFP[i] = (GenFP)wglGetProcAddress(glnames[i]);
 	glGenTextures(1, &tex_output);
-	((PFNGLACTIVETEXTUREPROC)glFP[1])(GL_TEXTURE0);
-	//((PFNGLBINDTEXTURESPROC)glFP[2])(GL_TEXTURE_3D, tex_output);
+	((PFNGLACTIVETEXTUREPROC)glFP[1])(GL_TEXTURE0); 
 	glBindTexture(GL_TEXTURE_3D, tex_output);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT); 
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	((PFNGLTEXIMAGE3DPROC)glFP[4])(GL_TEXTURE_3D, 0, GL_RGBA32F, SH_WIDTH, SH_HEIGHT, SH_DEPTH, 0, GL_RGBA, GL_FLOAT, NULL);
 	((PFNGLBINDIMAGETEXTUREPROC)glFP[5])(0, tex_output, 0, GL_TRUE, 0, GL_READ_WRITE, GL_RGBA32F);
 	GLuint ray_shader = ((PFNGLCREATESHADERPROC)(glFP[6])) (GL_COMPUTE_SHADER);
 	((PFNGLSHADERSOURCEPROC)glFP[7]) (ray_shader, 1, &shader1_cs, NULL);
 	((PFNGLCOMPILESHADERPROC)glFP[8]) (ray_shader);
-	GLuint ray_program = ((PFNGLCREATEPROGRAMPROC)(glFP[9])) ();
-	((PFNGLATTACHSHADERPROC)glFP[10]) (ray_program, ray_shader);
-	((PFNGLLINKPROGRAMPROC)glFP[11]) (ray_program);
-	((PFNGLGENFRAMEBUFFERSPROC)glFP[12]) (1, &FBO);
-	((PFNGLUSEPROGRAMPROC)glFP[13]) (ray_program);
-	
+	GLuint ray_program = ((PFNGLCREATEPROGRAMPROC)(glFP[9])) ();   
+	((PFNGLATTACHSHADERPROC)glFP[10]) (ray_program, ray_shader); 
+	((PFNGLLINKPROGRAMPROC)glFP[11]) (ray_program); 
+	((PFNGLGENFRAMEBUFFERSPROC)glFP[12]) (1, &FBO);           
+	((PFNGLUSEPROGRAMPROC)glFP[13]) (ray_program);       
+	 
 	
 	PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer = (PFNGLBINDFRAMEBUFFERPROC)glFP[14];
 	PFNGLFRAMEBUFFERTEXTURE3DPROC glFramebufferTexture3D = (PFNGLFRAMEBUFFERTEXTURE3DPROC)glFP[15];
 	PFNGLDISPATCHCOMPUTEPROC glDispatchCompute = (PFNGLDISPATCHCOMPUTEPROC)glFP[16];
 	PFNGLMEMORYBARRIERPROC glMemoryBarrier = (PFNGLMEMORYBARRIERPROC)glFP[17];
 	PFNGLBLITFRAMEBUFFERPROC glBlitFramebuffer = (PFNGLBLITFRAMEBUFFERPROC)glFP[18];
-	PFNGLUNIFORM1FPROC glUniform1f = (PFNGLUNIFORM1FPROC)glFP[19];
-	PFNGLUNIFORM1IPROC glUniform1i = (PFNGLUNIFORM1IPROC)glFP[20];
+	PFNGLUNIFORM1FPROC glUniform1f = (PFNGLUNIFORM1FPROC)glFP[19];    
+	PFNGLUNIFORM1IPROC glUniform1i = (PFNGLUNIFORM1IPROC)glFP[20];  
 	PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)glFP[21];
-	float time = 0;
-	int stage = 0;
+	float time = 0;  
+	int stage = 0; 
 	do {	
 		if(time < 10)
 		{
-			glClearColor(1, 1, 1, 1);
-			glClear ( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
+			//glClearColor(1, 1, 1, 1);
+			//glClear ( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
+			MMTime.u.sample = 0;
+			time = 0;
 		}
+		//Audio Stuff
+		waveOutGetPosition(hWaveOut, &MMTime, sizeof(MMTIME));
+		//_4klang_render(lpSoundBuffer);
+		//_4klang_render(buf);
+
 		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
 		glFramebufferTexture3D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_3D, tex_output, 0, 1);
 		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
@@ -97,10 +106,10 @@ int WINAPI WinMainCRTStartup()
 		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 		glBlitFramebuffer(0, 0, SH_WIDTH, SH_HEIGHT, 0, 0, SH_WIDTH, SH_HEIGHT, GL_COLOR_BUFFER_BIT, GL_LINEAR);
 		time+=1.;
-		glUniform1f(glGetUniformLocation(ray_program, VAR_TIME), time);
-		stage++; if(stage > 1){glUniform1i(glGetUniformLocation(ray_program, VAR_STAGE), 1); stage=0;}
+		glUniform1f(glGetUniformLocation(ray_program, VAR_TIME), (MMTime.u.sample/441000.0));
+		stage++; if(stage > 1 && stage < 3){glUniform1i(glGetUniformLocation(ray_program, VAR_STAGE), 1);}
 		SwapBuffers ( hDC );   
-	} while ( !GetAsyncKeyState(VK_ESCAPE) );
+	} while ( !GetAsyncKeyState(VK_ESCAPE) && MMTime.u.sample < (MAX_SAMPLES-(44100*5))*10);
    
-   	system("delete.bat");
+   	ExitProcess(0);
 }
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 CC   = gcc.exe
 CL   = crinkler.exe
-CLFLAGS = /OUT:demo.exe /PRIORITY:NORMAL /COMPMODE:INSTANT /HASHSIZE:100 /HASHTRIES:40 /TRANSFORM:CALLS  /SUBSYSTEM:WINDOWS /RANGE:opengl32 /RANGE:glu32 
-OBJ  = main.o
-LINKOBJ  = main.o
+CLFLAGS = /OUT:demo.exe /PRIORITY:NORMAL /COMPMODE:SLOW /HASHSIZE:100 /HASHTRIES:40 /TRANSFORM:CALLS  /SUBSYSTEM:WINDOWS /RANGE:opengl32 /RANGE:glu32 
+OBJ  = main.o 4klang/4klang.obj
+LINKOBJ  = main.o 4klang/4klang.obj
 CLLIBS = kernel32.lib user32.lib gdi32.lib opengl32.lib glu32.lib winmm.lib msvcrt.lib
 CLLIBPATH = /LIBPATH:"D:\Lib\10.0.19041.0\um\x86"
 LIBS =  -lmingw32 -lopengl32 -lglu32 -lgdi32 -lkernel32 -luser32 -lm -lwinmm -mwindows -nostdlib -lmsvcrt
@@ -23,7 +23,7 @@ clean: clean-custom
 
 # crinkler ...
 $(BIN): $(OBJ)
-	$(CL)  $(CLLIBPATH) $(CLFLAGS) $(CLLIBS) main.o
+	$(CL)  $(CLLIBPATH) $(CLFLAGS) $(CLLIBS) main.o 4klang/4klang.obj
     	
 # normal linking
 #$(BIN): $(OBJ)
